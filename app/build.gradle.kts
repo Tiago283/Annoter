8d749a6a5b19d69c384a12c0e6d1b2ca51b90eb1
Tiago283 <tiagogomesdasilva283@gmail.com>
2026-06-06 10:58:37 -0300

    Update and fix versions problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
 android {
     namespace = "com.tiago.annoter"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
     defaultConfig {
         applicationId = "com.tiago.annoter"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
